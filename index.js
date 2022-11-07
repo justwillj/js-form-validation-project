4c3ead0e3dc847aa84bba04ddff069a8c8f1beff
Justin Williams <williams.jman@gmail.com>
2022-11-07 14:43:39 -0500

    Cleaned up some code

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 /**
- * Checks through the input value too see if it contains letters or not
- * @param {string} value - The input string of the form the user is working on
- * @returns {boolean} - Gives a true of false value based off if there is letters or not
- */
-const containLetters = (value) => {
-  return /[a-zA-Z]/.test(value);
-};
-/**
- * Checks through the input value too see if it contains number or not
- * @param {number} value - The input string of the form the user is working on
- * @returns {boolean} - Gives a true of false value based off if there is numbers or not
- */
-const containNumber = (value) => {
-  return /[0-9]/.test(value);
-};
-/**
  * Checks through the input value too see if it white spaces or not
  * @param {string} value - The input string of the form the user is working on
  * @returns {boolean} - Gives a true of false value based off if there is numbers or not
@@ -235,6 +219,7 @@ const storeVariables = () => {
       element.addEventListener(
         "click",
         (formAdd = (element) => {
+          //Grabs the closest form to the validate button
           let currentForm = element.target.closest("form");
           //currentForm.addEventListener("submit", formValidationCheck);
 
