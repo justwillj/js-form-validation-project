64cd90e061d979d2bd15a4e131255bb8332aba60
Justin Williams <williams.jman@gmail.com>
2022-11-08 20:58:04 -0500

    Changed for loop

--- index.js
+++ index.js
@@ -279,8 +279,9 @@ const storeVariables = () => {
           }
 
           //Loops through the alphabetic class and run the validation test on it
-          for (let i = 0; i < testAlphabetic.length; i++) {
-            let values = testAlphabetic[i].value;
+          for (let alphabetic of testAlphabetic) {
+            let values = alphabetic.value;
+            console.log(values);
             letterCheck(values, formError);
           }
 
